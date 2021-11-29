17cdf6cfb2a45c9dbada24bd568d7cfaa4ce1e1e
Nik-Shalashov <monitor1@inbox.ru>
2021-11-30 01:03:04 +0700

    Добавил новые тесты

--- app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
@@ -1,7 +1,6 @@
 package com.geekbrains.myfirsttests
 
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Test
 
 class EmailValidatorTest {
@@ -39,5 +38,50 @@ class EmailValidatorTest {
     @Test
     fun emailValidator_NullEmail_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(null))
+    }
+
+    @Test
+    fun `проверка на наличие домена`() {
+        assertFalse(EmailValidator.isValidEmail("name@.com"))
+    }
+
+    @Test
+    fun `проверка на валидные символы в имени`() {
+        assertFalse(EmailValidator.isValidEmail("&<>$#@mail.com"))
+    }
+
+    @Test
+    fun `проверка на валидные символы в домене`() {
+        assertFalse(EmailValidator.isValidEmail("name@&$<>?.com"))
+    }
+
+    @Test
+    fun `проверка на не нулл`(){
+        assertNotNull(EmailValidator.isValidEmail("name@mail.com"))
+    }
+
+    @Test
+    fun `проверка на нулл`(){
+        assertNull(EmailValidator.isValidEmail(null))
+    }
+
+    @Test
+    fun `проверка на совпадение`() {
+        assertEquals(EmailValidator.isEqualsEmail(), "example@mail.com")
+    }
+
+    @Test
+    fun `проверка на несовпадение`() {
+        assertNotEquals(EmailValidator.isEqualsEmail(), "another@mail.com")
+    }
+
+    @Test
+    fun `проверка на совпадение массивов`() {
+        assertArrayEquals(EmailValidator.isArrayEmailsEquals(), arrayOf("num1@mail.com", "num2@mail.com", "num3@mail.com"))
+    }
+
+    @Test
+    fun `тест на одинаковый возвращаемый объект`(){
+        assertSame(EmailValidator.isSameTest1(), EmailValidator.isSameTest2())
     }
 }
